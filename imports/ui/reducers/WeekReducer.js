f4b447fb4f4db248dbb56af7639c00eca225418c
ben5_ <bwu86@uwo.ca>
2019-06-14 12:02:04 -0700

    Swapped DayReducer into WeekReducer

---
+++ imports/ui/reducers/WeekReducer.js
@@ -0,0 +1,77 @@
+import uuid from 'uuid';
+
+const timeState = {
+    eight : {
+        avail : false,
+        tasks : [],
+    },
+    nine : {
+        avail : false,
+        tasks : [],
+    },
+    ten : {
+        avail : false,
+        tasks : [],
+    },
+    eleven : {
+        avail : false,
+        tasks : [],
+    },
+    twelve : {
+        avail : false,
+        tasks : [],
+    },
+    one : {
+        avail : false,
+        tasks : [],
+    },
+    two : {
+        avail : false,
+        tasks : [],
+    },
+    three : {
+        avail : false,
+        tasks : [],
+    },
+    four : {
+        avail : false,
+        tasks : [],
+    },
+    five : {
+        avail : false,
+        tasks : [],
+    },
+}
+
+const initState = {
+    //eight corresponds with time 8:00 to 8:59
+    week : {
+        key: uuid.v4(),
+        monday : timeState,
+        tuesday : timeState,
+        wednesday : timeState,
+        thursday : timeState,
+        friday : timeState,
+        saturday : timeState,
+        sunday : timeState,
+    }
+}
+
+const WeekReducer = (state = initState, action) => {
+    switch(action.type) {
+        case 'TOGGLE_AVAIL':
+
+            return {
+                ...state,
+                times : {
+                    // action.time = !state.action.time
+                }
+            }
+        default:
+            return state
+    }
+}
+
+export default WeekReducer
+
+
