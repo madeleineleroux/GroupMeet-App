88460dd7e5cbb10cec99bff02ac738eb84e09b3b
p9i2b <madeleinemleroux@gmail.com>
2019-08-05 14:22:02 -0700

    got rid of group startup intialization

--- imports/server/main.js
+++ imports/server/main.js
@@ -12,21 +12,21 @@ Meteor.startup(() => {
     // if (Availability.find().count() === 0) {
     //     Availability.insert(initState);
     // }
-    let moment = require('moment');
-    moment.defaultFormat = "YYYYMMDD";
-    let start = (moment().startOf('week').format());
-
-    if (GroupSchedule.find().count() === 0) {
-        const allHours = booleanHours();
-        const gsInitState = Object.assign({}, allHours, {date: start});
-
-        GroupSchedule.insert(gsInitState);
-        let nextStart = (moment().add(1,'weeks').startOf('week').format());
-        const gsSecondWeek = Object.assign({}, allHours, {date: nextStart});
-        GroupSchedule.insert(gsSecondWeek);
-
-
-    }
+    // let moment = require('moment');
+    // moment.defaultFormat = "YYYYMMDD";
+    // let start = (moment().startOf('week').format());
+    //
+    // if (GroupSchedule.find().count() === 0) {
+    //     const allHours = booleanHours();
+    //     const gsInitState = Object.assign({}, allHours, {date: start});
+    //
+    //     GroupSchedule.insert(gsInitState);
+    //     let nextStart = (moment().add(1,'weeks').startOf('week').format());
+    //     const gsSecondWeek = Object.assign({}, allHours, {date: nextStart});
+    //     GroupSchedule.insert(gsSecondWeek);
+    //
+    //
+    // }
 
     if (Users.find().count() == 0) {
         Users.insert(
