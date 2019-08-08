cdb4eee4545b009af3d5aa88cb61fe73a896fdce
ben5_ <bwu86@uwo.ca>
2019-08-08 15:08:38 -0700

    Fixed grammar issue in group schedule

--- imports/ui/components/GroupView/GroupWeek.js
+++ imports/ui/components/GroupView/GroupWeek.js
@@ -47,7 +47,7 @@ class GroupWeek extends Tracker.Component {
                 <div className="triangle-right triangle" onClick={this.handleNextWeek}></div>
                 <div>
                     <h4>{this.props.groupSchedule.submitted.length + " member" +
-                    (this.props.groupSchedule.submitted.length > 1 ? "s have" : " has") + " submitted their schedule"} </h4>
+                    (this.props.groupSchedule.submitted.length > 1 || this.props.groupSchedule.submitted.length == 0 ? "s have" : " has") + " submitted their schedule"} </h4>
                 <div>
                     <div className="availableBox">Available</div>
                     <div className="busyBox">Busy</div>
