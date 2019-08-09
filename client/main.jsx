c37d227e3ee7fe680a0e5e2ff9f88fc7f5f5dead
ben5_ <bwu86@uwo.ca>
2019-08-09 15:50:01 -0700

    Removed fatal error where redux devtools is required

--- client/main.jsx
+++ client/main.jsx
@@ -17,7 +17,7 @@ import {fetchGroup} from "../imports/ui/actions/GroupAction";
 const store = createStore(reducers,
   compose(
       applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ));
 
 store.dispatch(fetchSchedule());
